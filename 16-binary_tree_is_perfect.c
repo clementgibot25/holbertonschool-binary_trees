b8200a27b8f26759a4560c20a37ff1efd6db6b4e
clementgibot25 <10500@holbertonstudents.com>
2025-04-08 14:47:20 +0200

    add the call to function 9 and 12

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
-
+#include "9-binary_tree_height.c"
+#include "12-binary_tree_leaves.c"
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to check
