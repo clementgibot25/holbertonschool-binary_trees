7abebcfa7a3798cc87c16eafffe583cba8d48caf
clementgibot25 <10500@holbertonstudents.com>
2025-04-08 14:38:20 +0200

    fix pointer argument name

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,20 +2,20 @@
 
 /**
  * binary_tree_sibling - Finds the sibling of a node
- * @tree: Pointer to the node to find the sibling
+ * @node: Pointer to the node to find the sibling
  *
  * Return: Pointer to the sibling node, or NULL if none
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *tree)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (tree == NULL || tree->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (tree->parent->left == tree)
-		return (tree->parent->right);
+	if (node->parent->left == node)
+		return (node->parent->right);
 
-	if (tree->parent->right == tree)
-		return (tree->parent->left);
+	if (node->parent->right == node)
+		return (node->parent->left);
 
 	return (NULL);
 }
