2b3cdbec50fada5568cf9c3f040ac570bfd52c31
clementgibot25 <10500@holbertonstudents.com>
2025-04-08 14:24:29 +0200

    fix function to check balance of tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,5 +11,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	int left = (int)binary_tree_height(tree->left);
+	int right = (int)binary_tree_height(tree->right);
+
+	return (left - right);
 }
