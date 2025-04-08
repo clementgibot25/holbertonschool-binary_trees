1177771bc9ca9f3d5485c9e3e4e98cbde2475920
TilawatArnaud <tilawatarnaud@gmail.com>
2025-04-08 13:49:56 +0200

    pair review task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	if (tree->left != NULL)
-		left_height = binary_tree_height(tree->left);
-
-	if (tree->right != NULL)
-		right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
