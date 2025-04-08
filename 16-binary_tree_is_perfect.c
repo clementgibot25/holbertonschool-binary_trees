79865b8bae0b0212c8e1378342edac5cc927a675
clementgibot25 <10500@holbertonstudents.com>
2025-04-08 09:39:55 +0200

    add tasks 14 16 18 to my branch for team review before merge

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_height(tree) == binary_tree_leaves(tree));
+}
