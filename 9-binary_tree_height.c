6a17c47387fc4a353ba770bc6b6a68aee9d1890f
clementgibot25 <10500@holbertonstudents.com>
2025-04-10 11:15:38 +0200

    fix function to return 0 instead of -1

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,15 +5,13 @@
  * @tree: pointer to the root node of the tree to measure the height
  * Return: height of the tree, or 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
-
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	int left = binary_tree_height(tree->left);
+	int right = binary_tree_height(tree->right);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	return (1 + (left > right ? left : right));
 }
